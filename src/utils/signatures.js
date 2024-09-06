31413cf85aa5983e3496f22115e02605b633ef51
SumitCMLab <sb152377@gmail.com>
2024-09-06 17:06:24 +0530

    repo cleanup

---
+++ src/utils/signatures.js
@@ -0,0 +1,32 @@
+const { ethers } = require('ethers');
+
+const privateKey = '<yourprivatekey';
+const wallet = new ethers.Wallet(privateKey);
+
+
+async function signData(data){
+    const signature =  await wallet.signMessage(JSON.stringify(data));
+
+    console.log('Signature:', signature);
+    // return signature;
+}
+
+async function verifysign(data, signature, from){
+    const address = ethers.recoverAddress(ethers.hashMessage(JSON.stringify(data)), signature);
+    console.log(address);
+    if(address.toLowerCase() === from.toLowerCase())
+        return address;
+    return 0;
+}
+
+// signData({
+//     "hello": "Sumit"
+// });
+
+// verifysign({
+//     "hello": "Sumit"
+// }, "0xcf2276c1f141920c54fbb506cfefccef364281c32696549e49bf52d5e163a54c6a16b94183790ce174e7cb3aadc97b8a78840ce10a5d5a5b8c79e007e50690721b")
+
+module.exports = {
+    verifysign
+}
